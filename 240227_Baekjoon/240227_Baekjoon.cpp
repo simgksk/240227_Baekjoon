1e54512b45a3a829df705911da80a5244336a74c
하나 <123716081+simgksk@users.noreply.github.com>
2024-02-27 23:46:01 +0900

    15059. Hard choice

--- 240227_Baekjoon/240227_Baekjoon.cpp
+++ 240227_Baekjoon/240227_Baekjoon.cpp
@@ -4,4 +4,34 @@ using namespace std;
 
 	int main()
 	{
+#pragma region 15059. Hard choice
+
+		int ca, ba, pa, cr, br, pr;
+		int result = 0;
+
+		cin >> ca >> ba >> pa >> cr >> br >> pr;
+
+		if(ca<cr)
+		{
+			int c = cr - ca;
+			result += c;
+		}
+
+		if (ba < br)
+		{
+			int b = br - ba;
+			result += b;
+		}
+
+		if (pa < pr)
+		{
+			int p = pr - pa;
+			result += p;
+		}
+
+		cout << result;
+
+#pragma endregion
+
+		return 0;
 	}
